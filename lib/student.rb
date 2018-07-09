0ca8a0a79284a67f239f1bc0946187e93c69cad2
Eric Fiorani <efiorani305@gmail.com>
2018-07-09 19:23:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,7 +79,7 @@ class Student
     DB[:conn].execute(students)
   end
 
-  def self.first_X_students_in_grade_10
+  def first_X_students_in_grade_10
     students = <<-SQL
       SELECT * FROM students WHERE grade = 10
       LIMIT ?
