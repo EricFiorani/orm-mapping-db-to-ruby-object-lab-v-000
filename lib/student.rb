1cec6aef7242c6709eca198faeff2454b688d205
Eric Fiorani <efiorani305@gmail.com>
2018-07-09 19:25:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,4 +86,26 @@ class Student
       SQL
     DB[:conn].execute(students, num).map { |row| self.new_from_db(row)  }
   end
+
+  def self.first_student_in_grade_10
+    students = <<-SQL
+    SELECT * FROM students WHERE grade = 10
+    ORDER BY students.id
+    LIMIT 1
+    SQL
+    DB[:conn].execute(students).map do |row|
+       self.new_from_db(row)
+     end.first
+  end
+
+
+  def self.all_students_in_grade_X(grade)
+    students = <<-SQL
+    SELECT * FROM students WHERE grade = ?
+    ORDER BY students.id
+    SQL
+
+    DB[:conn].execute(students, grade).map { |row| self.new_from_db(row)  }
+
+  end
 end
